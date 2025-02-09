fb132b517d6deb7f5ad0e6c0685b5a32177f8793
Guilherme Assis <assisguilhermefernandes@gmail.com>
2025-02-09 19:49:09 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("O pino redondo r5 se encaixa no furo redondo r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile.
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("A cavilha quadrada w2 cabe no furo redondo r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("O pino quadrado w20 não cabe no furo redondo r5.");
+        }
+    }
+}
